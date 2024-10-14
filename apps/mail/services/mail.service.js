33f95f773630b2b76c4ea1a54051bf9364dfbfec
Valery Vishnevezky <valery020201@gmail.com>
2024-10-14 16:04:11 +0300

    Add mail service with basic data

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,158 @@
 // mail service
+import { storageService } from './services/async-storage.service.js'
+import { utilService } from './services/util.service.js'
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save,
+    getDefaultFilter,
+    // getFilterFromSearchParams,
+}
+const MAIL_KEY = 'bookDB'
+_createMails()
+
+function query(filterBy = {}) {
+    return storageService.query(MAIL_KEY)
+        .then(mails => {
+            if (filterBy.subject) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                mails = mails.filter(mail => regExp.test(mail.subject))
+            }
+            if (filterBy.body) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                mails = mails.filter(mail => regExp.test(mail.body))
+            }
+            if (filterBy.isRead) {
+                mails = mails.filter(mail => mail.isRead === filterBy.isRead)
+            }
+            return mails
+        })
+}
+
+function get(mailId) {
+    return storageService.get(MAIL_KEY, mailId)//.then(_setNextPrevMailId)
+}
+
+function remove(mailId) {
+    return storageService.remove(MAIL_KEY, mailId)
+}
+
+function save(mail) {
+    if (mail.id) {
+        return storageService.put(MAIL_KEY, mail)
+    } else {
+        return storageService.post(MAIL_KEY, mail)
+    }
+}
+
+function getDefaultFilter() {
+    return { subject: '', body: 0, isRead: false }
+}
+
+// function getFilterFromSearchParams(searchParams) {
+//     const subject = searchParams.get('subject') || ''
+//     const body = searchParams.get('body') || ''
+//     const isRead = searchParams.get('isRead') || ''
+//     return {
+//         subject,
+//         body,
+//         isRead
+//     }
+// }
+
+// function _setNextPrevMailId(mail) {
+//     return query().then((mails) => {
+//         const mailIdx = mails.findIndex((currMail) => currMail.id === mail.id)
+//         const nextMail = mails[mailIdx + 1] ? mails[mailIdx + 1] : mails[0]
+//         const prevMail = mails[mailIdx - 1] ? mails[mailIdx - 1] : mails[mails.length - 1]
+//         mail.nextMailId = nextMail.id
+//         mail.prevMailId = prevMail.id
+//         return mail
+//     })
+// }
+
+function _createMails() {
+    let mails = utilService.loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length) {
+        mails = [{
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'lili@appsus.com',
+            "to": 'user@appsus.com'
+        },
+        {
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'appsus@appsus.com',
+            "to": 'user@appsus.com'
+        },
+        {
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'appsus@appsus.com',
+            "to": 'user@appsus.com'
+        },
+        {
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'appsus@appsus.com',
+            "to": 'user@appsus.com'
+        },
+        {
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'appsus@appsus.com',
+            "to": 'user@appsus.com'
+        },
+        {
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'appsus@appsus.com',
+            "to": 'user@appsus.com'
+        },
+        {
+            "id": 'e101',
+            "createdAt": 1551133930500,
+            "subject": 'Miss you!',
+            "body": 'Would love to catch up sometimes',
+            "isRead": false,
+            "sentAt": 1551133930594,
+            "removedAt": null,
+            "from": 'appsus@appsus.com',
+            "to": 'user@appsus.com'
+        },]
+        utilService.saveToStorage(MAIL_KEY, mail)
+    }
+}
