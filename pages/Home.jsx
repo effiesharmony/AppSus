193fea91f3ed92c745d66e68fd7a2652abd73c1c
Valery Vishnevezky <valery020201@gmail.com>
2024-10-20 02:15:52 +0300

    Small fix

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,4 +1,3 @@
-
 export function Home() {
     return (
         <React.Fragment>
@@ -6,10 +5,6 @@ export function Home() {
                 <img className="logo-img-white" src="logo.imgs/logo-white.png" alt="AppSus logo" />
                 <p>the place to <span className="home-keep">keep</span> and <span className="home-share">share</span> your life<span>.</span></p>
             </section>
-            <div className="background-img">
-                {/* <img src="assets/img/background-img.avif" /> */}
-            </div>
-            <img src="../assets/img/background.jpg" alt="" />
         </React.Fragment>
     )
 }
