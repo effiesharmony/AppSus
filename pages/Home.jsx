ce3385161fc096c1ac7fd5a58f98d928a4e79ccf
Effie Armony <harmony.effie@gmail.com>
2024-10-20 02:04:33 +0300

    Style homepage

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,8 +3,8 @@ export function Home() {
     return (
         <React.Fragment>
             <section className="home">
-                <img src="" alt="" />
-                <p>the place to <span className="home-keep">keep</span> and <span className="home-share">share</span> your life</p>
+                <img className="logo-img-white" src="logo.imgs/logo-white.png" alt="AppSus logo" />
+                <p>the place to <span className="home-keep">keep</span> and <span className="home-share">share</span> your life<span>.</span></p>
             </section>
             <div className="background-img">
                 {/* <img src="assets/img/background-img.avif" /> */}
