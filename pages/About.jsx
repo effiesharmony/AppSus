0d1ab445eecc501275fd33a3e82b3aa8eeca2454
Valery Vishnevezky <valery020201@gmail.com>
2024-10-20 00:52:55 +0300

    Add about

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,15 @@
-
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
+  return (
+    <section className="about">
+      <div className="about-valery">
+        <img src="/about.img/valery.jpg" alt="about-valery" />
+        <h1>Valery Vishnevezky</h1>
+        <h2>Mother of cats, breaker of awkward silences, and creator of the mail app</h2>
+      </div>
+      <div className="about-effie"></div>
+      <img src="/about.img/valery.jpg" alt="about-valery" />
+        <h1>Effie Armony</h1>
+        <h2>Mother of cats, breaker of awkward silences, and creator of the notes app</h2>
     </section>
+  );
 }
