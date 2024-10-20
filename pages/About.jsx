cd966b4f82bbfd9434d0abf5322821832beb037c
Effie Armony <harmony.effie@gmail.com>
2024-10-20 08:30:50 +0300

    fix bug?

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,12 +2,12 @@ export function About() {
   return (
     <section className="about">
       <div className="about-valery">
-        <img src="/about.img/valery.jpg" alt="about-valery" />
+        <img src="assets/img/valery.jpg" alt="about-valery" />
         <h1>Valery Vishnevezky</h1>
         <h2>Mother of unfinished coffee mugs, debugger of infinite loops, and creator of the mail app</h2>
       </div>
       <div className="about-effie">
-      <img src="/about.img/effie.jpeg" alt="about-valery" />
+      <img src="assets/img/effie.jpeg" alt="about-effie" />
         <h1>Effie Armony</h1>
         <h2>Mother of cats, breaker of awkward silences, and creator of the notes app</h2>
       </div>
